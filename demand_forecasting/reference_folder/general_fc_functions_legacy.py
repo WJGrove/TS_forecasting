1b56c8d268116bcee4085c2cf816521aaea0c5ce
WJGrove <wj.grove.data@gmail.com>
2025-11-29 17:52:34 -0600

    formatting

--- demand_forecasting/reference_folder/general_fc_functions_legacy.py
+++ demand_forecasting/reference_folder/general_fc_functions_legacy.py
@@ -507,14 +507,24 @@ def boxcox_multi_ts_sps(
 # Define the inverse Box-Cox transformation function
 @pandas_udf(schema, PandasUDFType.GROUPED_MAP)
 def inverse_boxcox_transform(pdf):
-    time_series_id = pdf["time_series_id"].iloc[0]  # Assuming time_series_id is uniform within each group
-    lam_info = lambda_values_dict.get(time_series_id, {"fitted_lambda": None, "is_transformed": False})
+    time_series_id = pdf["time_series_id"].iloc[
+        0
+    ]  # Assuming time_series_id is uniform within each group
+    lam_info = lambda_values_dict.get(
+        time_series_id, {"fitted_lambda": None, "is_transformed": False}
+    )
 
     if lam_info["is_transformed"]:
         lam = lam_info["fitted_lambda"]
-        pdf["y_hat"] = pdf["y_hat"].apply(lambda x: inv_boxcox(x, lam) if pd.notnull(x) else x)
-        pdf["y_hat_upper"] = pdf["y_hat_upper"].apply(lambda x: inv_boxcox(x, lam) if pd.notnull(x) else x)
-        pdf["y_hat_lower"] = pdf["y_hat_lower"].apply(lambda x: inv_boxcox(x, lam) if pd.notnull(x) else x)
+        pdf["y_hat"] = pdf["y_hat"].apply(
+            lambda x: inv_boxcox(x, lam) if pd.notnull(x) else x
+        )
+        pdf["y_hat_upper"] = pdf["y_hat_upper"].apply(
+            lambda x: inv_boxcox(x, lam) if pd.notnull(x) else x
+        )
+        pdf["y_hat_lower"] = pdf["y_hat_lower"].apply(
+            lambda x: inv_boxcox(x, lam) if pd.notnull(x) else x
+        )
 
     return pdf
 
