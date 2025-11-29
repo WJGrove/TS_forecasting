44f9b8cc98ebb877e4b5677d0192c9766c7b2aff
WJGrove <wj.grove.data@gmail.com>
2025-11-29 12:55:08 -0600

    added import and spark session instance to job file for testing outside of Databricks

--- demand_forecasting/ts_preprocessing_job.py
+++ demand_forecasting/ts_preprocessing_job.py
@@ -1,11 +1,22 @@
-from ts_preprocessing import TSPreprocessingConfig, TSPreprocessor
-import time
 from pyspark.sql import SparkSession
 
+# Create a local SparkSession
+spark = (
+    SparkSession.builder.appName("ts-preprocessing-job")
+    .master("local[*]")  # use all local cores
+    .getOrCreate()
+)
+
+# Everything above this line is only needed for local testing and debugging locally, outside of Databricks.
+
+from ts_preprocessing import TSPreprocessingConfig, TSPreprocessor
+import time
+
+
 config = TSPreprocessingConfig(
-    source_table="forecast_dev.data_science.sales_forecast_source_view",
+    source_table="forecast_dev.data_science.ts_source_view",
     output_catalog="forecast_dev.data_science",
-    output_table_name="sales_preprocessed",
+    output_table_name="ts_preprocessed",
 )
 
 pre = TSPreprocessor(spark, config)
